fa9868f98f7c6ef3d8d050f3699ffd7e887a95f0
Evan Derby <somekidwithhtml@gmail.com>
2017-10-21 13:05:21 -0400

    Update to serve index.html

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 import modules.reddit as red
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def index():
-    return "Hello world"
+    return app.send_static_file("index.html");
 
 @app.route("/reddit")
 def reddit():
