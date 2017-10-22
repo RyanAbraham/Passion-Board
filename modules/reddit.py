c2c109e719546ce54e246304f2a9d68333a44ac2
Ryan Abraham <ryan_ja@live.ca>
2017-10-21 21:14:44 -0400

    Updated routing and reddit module

--- modules/reddit.py
+++ modules/reddit.py
@@ -8,16 +8,19 @@ from collections import Counter
 indicoio.config.api_key = config.indico_api_key
 MAX_POSTS, MAX_COMMENTS = 10, 2
 
-def fetchposts(subreddit, target_emotion):
+def fetch_posts(subreddit, target_emotion):
     submissions = {}
     return_data = {}
     return_data['submissions'] = []
 
+    # Connect to Reddit via PRAW
     reddit = praw.Reddit(client_id='Us-byLFTjQmSJQ',
                          client_secret=config.reddit_client_secret,
                          user_agent='python:com.hackharvard.sentient-dashboard:v1.0')
 
+    # Fetch top X posts from hot of chosen subreddit
     for submission in reddit.subreddit(subreddit).hot(limit=MAX_POSTS):
+        # Run emotional analysis on each emotion
         emotion_sum = Counter(indicoio.emotion(submission.title))
         for counter, comment in enumerate(submission.comments):
             if counter >= MAX_COMMENTS:
@@ -31,9 +34,11 @@ def fetchposts(subreddit, target_emotion):
             emotion_sum[emotion] /= counter+1
         submissions[submission.title] = [emotion_sum, submission.shortlink]
 
+    # Cut out all emotions except the chosen one
     for sub in submissions:
         submissions[sub][0] = submissions[sub][0][target_emotion]
 
+    # Construct JSON to return
     for post in reversed(sorted(submissions, key=submissions.get)):
         return_data['submissions'].append({'title': post, 'score': submissions[post][0], 'shortlink': submissions[post][1]})
     return json.dumps(return_data)
---
+++ modules/spotify.py
@@ -0,0 +1,8 @@
+import json
+playlist_uris = {
+    "joy": [
+        "https://open.spotify.com/user/spotify/playlist/37i9dQZF1DXdPec7aLTmlC"
+    ], ""
+}
+
+def fetch_playlist_uri(emotion):
